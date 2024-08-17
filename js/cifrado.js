95c83cb4d6c1a542bd38249df00c4fd82a732602
erika <erikaojanama36@gmail.com>
2024-08-16 20:01:01 -0500

    agregando botones

--- js/cifrado.js
+++ js/cifrado.js
@@ -1,4 +1,4 @@
-function cifrado(texto,desplazamiento){
+function descifrado_cifrado(texto,desplazamiento,accion){
     const abecedario = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let resultado = '';
 
@@ -9,13 +9,23 @@ function cifrado(texto,desplazamiento){
         if(indiceActual === -1){
             resultado += letraActual;
         }else{
-            let nuevoIndice = (indiceActual + desplazamiento)%abecedario.length;
+            let nuevoIndice
+            if(accion === 1){
+                nuevoIndice = (indiceActual + desplazamiento)%abecedario.length;
+            }else{
+                nuevoIndice = (indiceActual - desplazamiento)%abecedario.length;
+                if(nuevoIndice < 0){
+                    nuevoIndice += abecedario.length;
+                }
+            }
             resultado += abecedario[nuevoIndice];
         }
     }
     return resultado;
 }
-let textoOriginal = "Hola";
-let desplazamiento = 25;
-let textoCifrado = cifrado(textoOriginal,desplazamiento)
-console.log(textoCifrado);
+/*let textoOriginal = "ZGDS";
+let desplazamiento = 18;
+let textoCifrado = descifrado_cifrado(textoOriginal,desplazamiento,0);
+console.log(textoCifrado);*/
+
+export default descifrado_cifrado;
---
+++ js/script.js
@@ -0,0 +1,20 @@
+import descifrado_cifrado from "./cifrado.js";
+
+const mensaje = document.getElementById("mensaje");
+const desplazamiento = document.getElementById("desplazamiento");
+const boton_cifrar = document.getElementById("cifrar");
+const boton_descifrar = document.getElementById("descifrar");
+
+boton_cifrar.addEventListener("click",()=>{
+    if(mensaje.value == ""){
+        alert("Ingresa tu mensaje secreto");
+    }else if(desplazamiento.value ==""){
+        alert("Ingresa tu clave secreta");
+    }else{
+        let texto = "" ;
+        let textoCifrado = descifrado_cifrado(mensaje.value,desplazamiento.value,1);
+        texto += "El mensaje cifrado es: "
+        texto += textoCifrado
+        document.getElementById("resultado").innerHTML = texto;
+    }
+})
